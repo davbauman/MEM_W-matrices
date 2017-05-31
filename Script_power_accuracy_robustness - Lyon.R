7368a866500cb2f8f1fd5f6e315fd7ef6e6f499b
davbauman <dbauman@ulb.ac.be>
2017-05-31 20:12:10 +0200

    Ok untill MEM generation

--- Script_power_accuracy_robustness - Lyon.R
+++ Script_power_accuracy_robustness - Lyon.R
@@ -171,6 +171,12 @@ resultsM_pop[30, c(1010:2009, 3010:4009)] <- R2_pop_med
 # Begining of the simulation process:
 # ***********************************
 
+   ######################
+   ######################
+   ### I. Broad scale ###
+   ######################
+   ######################
+
 for (i in 1:nperm) {
 
 # Sampling scheme:
@@ -257,8 +263,11 @@ C <- xy[sort, ]
 
 # We keep the lines of MEM that correspond to the sampled cells ('tri'):
 # **********************************************************************
-
 MEMsub <- MEM[sort, ]
+
+# We sample the response variable within the sampled cells ('y_sub'):
+# *******************************************************************
+y_sub <- y_broad[sort, ]
 
 # Visualisation:
 # **************
@@ -268,3 +277,107 @@ MEMsub <- MEM[sort, ]
 
 # Construction of the different W matrices:
 # #########################################
+
+   # Connectivity matrices (B):
+   # **************************
+
+Y.del <- tri2nb(C) 
+Y.gab <- graph2nb(gabrielneigh(as.matrix(C), nnmult = 5), sym = TRUE)
+Y.rel <- graph2nb(relativeneigh(as.matrix(C), nnmult = 5), sym = TRUE)
+Y.mst <- mst.nb(xy.d1)
+
+   # Distance-based B (radius around points):
+lowlim <- give.thresh(xy.d1)
+uplim <- max(xy.d1)
+thresh <- seq(lowlim, uplim, le = 3)   # 3 tested distances
+Y.listDB <- lapply(thresh, dnearneigh, x = as.matrix(C), d1 = 0)
+
+# Weighting functions and fixed parametres:
+# *****************************************
+f1 <- function (D, dmax)    {1-(D/dmax)}        # Linear function
+f2 <- function (D, dmax, y) {1-(D/dmax)^y}      # Concave-down function
+f3 <- function (D, y)       {1/(D^y)}           # Concave-up function
+f4 <- function (D, t)       {1-(D/(4*t))^2}     # PCNM criterion
+
+max.del <- max(unlist(nbdists(Y.del, as.matrix(C)))) 
+max.gab <- max(unlist(nbdists(Y.gab, as.matrix(C))))
+max.rel <- max(unlist(nbdists(Y.rel, as.matrix(C)))) 
+max.mst <- max(unlist(nbdists(Y.mst, as.matrix(C))))
+
+nbdist <- lapply(Y.list10dnn, coords = as.matrix(C), nbdists)
+unlist <- lapply(nbdist, unlist)
+max.DB.list <- lapply(unlist, max)
+
+# Generation of MEM variables:
+# ****************************
+# del:
+# ****
+Y.del.MEM <- test.W.R2(nb = Y.del, xy = C, MEM.autocor = MEM_model)
+Y.del.MEM.f1 <- test.W.R2(nb = Y.del, xy = C, f = f1, dmax = max.del, 
+                          MEM.autocor = MEM_model)
+Y.del.MEM.f2 <- test.W.R2(Y = y_sub, nb = Y.del, xy = C, MEM.autocor = MEM_model, 
+                          f = f2, dmax = max.del, y = 2:10)
+Y.del.MEM.f3 <- test.W.R2(Y = y_sub, nb = Y.del, xy = C, MEM.autocor = MEM_model, 
+                          f = f3, y = 2:10)
+# gab:
+# ****
+Y.gab.MEM <- test.W.R2(nb = Y.gab, xy = C, MEM.autocor = MEM_model)
+Y.gab.MEM.f1 <- test.W.R2(nb = Y.gab, xy = C, f = f1, dmax = max.gab, 
+                          MEM.autocor = MEM_model)
+Y.gab.MEM.f2 <- test.W.R2(Y = y_sub, nb = Y.gab, xy = C, MEM.autocor = MEM_model, 
+                          f = f2, dmax = max.gab, y = 2:10)
+Y.gab.MEM.f3 <- test.W.R2(Y = y_sub, nb = Y.gab, xy = C, MEM.autocor = MEM_model, 
+                          f = f3, y = 2:10)
+# rel:
+# ****
+Y.rel.MEM <- test.W.R2(nb = Y.rel, xy = C, MEM.autocor = MEM_model)
+Y.rel.MEM.f1 <- test.W.R2(nb = Y.rel, xy = C, f = f1, dmax = max.rel, 
+                          MEM.autocor = MEM_model)
+Y.rel.MEM.f2 <- test.W.R2(Y = y_sub, nb = Y.rel, xy = C, MEM.autocor = MEM_model, 
+                          f = f2, dmax = max.rel, y = 2:10)
+Y.rel.MEM.f3 <- test.W.R2(Y = y_sub, nb = Y.rel, xy = C, MEM.autocor = MEM_model, 
+                          f = f3, y = 2:10)
+# mst:
+# ****
+Y.mst.MEM <- test.W.R2(nb = Y.mst, xy = C, MEM.autocor = MEM_model)
+Y.mst.MEM.f1 <- test.W.R2(nb = Y.mst, xy = C, f = f1, dmax = max.mst, 
+                          MEM.autocor = MEM_model)
+Y.mst.MEM.f2 <- test.W.R2(Y = y_sub, nb = Y.mst, xy = C, MEM.autocor = MEM_model, 
+                            f = f2, dmax = max.mst, y = 2:10)
+Y.mst.MEM.f3 <- test.W.R2(Y = y_sub, nb = Y.mst, xy = C, MEM.autocor = MEM_model, 
+                            f = f3, y = 2:10)
+# DB:
+# ***
+Y.DB1.MEM <- test.W.R2(nb = Y.listDB[[1]], xy = C, MEM.autocor = MEM_model)
+Y.DB1.MEM.f1 <- test.W.R2(nb = Y.listDB[[1]], xy = C, f = f1, dmax = max.DB.list[[1]], 
+                          MEM.autocor = MEM_model)
+Y.DB1.MEM.f2 <- test.W.R2(Y = y_sub, nb = Y.listDB[[1]], xy = C, 
+                          MEM.autocor = MEM_model, f = f2, dmax = max.DB.list[[1]], 
+                          y = 2:10)
+Y.DB1.MEM.f3 <- test.W.R2(Y = y_sub, nb = Y.listDB[[1]], xy = C, 
+                          MEM.autocor = MEM_model, f = f3, y = 2:10)
+
+Y.DB2.MEM <- test.W.R2(nb = Y.listDB[[2]], xy = C, MEM.autocor = MEM_model)
+Y.DB2.MEM.f1 <- test.W.R2(nb = Y.listDB[[2]], xy = C, f = f1, dmax = max.DB.list[[2]], 
+                          MEM.autocor = MEM_model)
+Y.DB2.MEM.f2 <- test.W.R2(Y = y_sub, nb = Y.listDB[[2]], xy = C, 
+                          MEM.autocor = MEM_model, f = f2, dmax = max.DB.list[[2]], 
+                          y = 2:10)
+Y.DB2.MEM.f3 <- test.W.R2(Y = y_sub, nb = Y.listDB[[2]], xy = C, 
+                          MEM.autocor = MEM_model, f = f3, y = 2:10)
+
+Y.DB3.MEM <- test.W.R2(nb = Y.listDB[[3]], xy = C, MEM.autocor = MEM_model)
+Y.DB3.MEM.f1 <- test.W.R2(nb = Y.listDB[[3]], xy = C, f = f1, dmax = max.DB.list[[3]], 
+                          MEM.autocor = MEM_model)
+Y.DB3.MEM.f2 <- test.W.R2(Y = y_sub, nb = Y.listDB[[3]], xy = C, 
+                          MEM.autocor = MEM_model, f = f2, dmax = max.DB.list[[3]], 
+                          y = 2:10)
+Y.DB3.MEM.f3 <- test.W.R2(Y = y_sub, nb = Y.listDB[[3]], xy = C, 
+                          MEM.autocor = MEM_model, f = f3, y = 2:10)
+
+# DBMEM with PCNM criteria:
+# *************************
+Y.DB.PCNM <- test.W.R2(nb = Y.listDB[[1]], xy = C, f = f4, t = lowlim, 
+                       MEM.autocor = MEM_model)
+
+
