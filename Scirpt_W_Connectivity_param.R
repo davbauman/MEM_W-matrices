911f4fb2d25b0c8314eca48424079e485c3d8423
davbauman <dbauman@ulb.ac.be>
2017-06-07 16:32:34 +0200

    MEM_W_Connectivity_param
    Sur une grille de 500 x 250 cellules. Non-superposition des sampled
    quadrats réarrangée.

--- Scirpt_W_Connectivity_param.R
+++ Scirpt_W_Connectivity_param.R
@@ -2,6 +2,8 @@
 ### Connectivity parameters of the different connectivity matrices ###
 # ****************************************************************** #
 
+rm(list = ls())
+
 # Usefull packages and functions:
 # *******************************
 
@@ -39,10 +41,7 @@ connect_param[8, ] <- c(1:ncol(connect_param))
 # The MEM are built for a full grid (50 x 25 cells):
 # **************************************************
 
-xy <- expand.grid(x = seq(1, 50, 1), y = seq(1, 25, 1))
-nb <- cell2nb(nrow = 25, ncol = 50, "queen")
-nb2 <- nb2listw(nb)
-MEM <- scores.listw(nb2, MEM.autocor = MEM_model)
+xy <- expand.grid(x = seq(1, 500, 1), y = seq(1, 250, 1))
 
 # ***************************************************************** #
 ### I. The response remains unchanged and the sampling design varies:
@@ -56,12 +55,12 @@ for (i in 1:nperm) {
   if (design == "clustered") {
     C <- as.matrix(matrix(0, ncol = 2, nrow = 117))
     set.seed(i)
-    x1 <- runif(39, min = 2, max = 14)
-    y1 <- runif(39, min = 17, max = 25)
-    x2 <- runif(39, min = 19, max = 34)
-    y2 <- runif(39, min = 15, max = 25)
-    x3 <- runif(39, min = 30, max = 50)
-    y3 <- runif(39, min = 1, max = 10)
+    x1 <- runif(39, min = sample(c(1:50), 1), max = sample(c(120:140), 1))
+    y1 <- runif(39, min = sample(c(130:170), 1), max = sample(c(220:250), 1))
+    x2 <- runif(39, min = sample(c(180:210), 1), max = sample(c(270:310), 1))
+    y2 <- runif(39, min = sample(c(130:170), 1), max = sample(c(220:250), 1))
+    x3 <- runif(39, min = sample(c(330:380), 1), max = sample(c(450:498), 1))
+    y3 <- runif(39, min = sample(c(1:50), 1), max = sample(c(100:150), 1))
     
     C[, 1] <- rbind(x1, x2, x3)
     C[, 2] <- rbind(y1, y2, y3)
@@ -70,29 +69,20 @@ for (i in 1:nperm) {
     
     C <- as.matrix(matrix(0, ncol = 2, nrow = 117))
     set.seed(i)
-    C[, 1] <- runif(117, min = 1, max = 50)
-    C[, 2] <- runif(117, min = 1, max = 25) 
+    C[, 1] <- runif(117, min = 1, max = 498)
+    C[, 2] <- runif(117, min = 1, max = 250) 
     
   }
   
   # Attribute each sampled point to one of the grid cells:
   # ******************************************************
   
-  # /25 = taille d'un côté du quadrat ; pour que la numérotation des quadrats se
-  # fasse de gauche à droite en partant du coin inférieur gauche de la grille :
-  # N <- y * (nb horizontal de quadrats) + x + 1 ; pour que la numérotation des 
-  # quadrats se fasse de bas en haut en partant du coin inférieur gauche :
-  # N <- x * (nb vertical de quadrats) + y + 1
-  
-  #  # Comme les coord. sont comprises entre 0.02 et 1 et 0.02 et 0.5, on les multiplie
-  #  # par 50 (momentanément) pour attributer une cellule à chaque point.
-  
   grid.size <- 1
   tri <- c()
   for (k in 1:nrow(C)) {
     x <- floor((C[k, 1]) / (grid.size))
     y <- floor((C[k, 2]) / (grid.size))
-    N <- y * 50 + x + 1
+    N <- y * 500 + x + 1
     tri <- c(tri, N)
   }
   
@@ -106,25 +96,21 @@ for (i in 1:nperm) {
   control <- length(levels(as.factor(sort)))
   while (control != nrow(C)) {
     cible <- c()
-    for(k in 1:(length(sort)-1)) if (sort[k+1] == sort[k]) cible <- c(cible, k+1)
-    for (k in cible) {
-      if (length(which(seq(50, 1250, 50) == sort[k])) == 0) {
-        sort[k] = sort[k] + 1
-      } else {
-        sort[k] = sort[k] - 1
-      }
+    for (k in 1:(length(sort)-1)) if (sort[k+1] == sort[k]) cible <- c(cible, (k+1))
+    for (x in cible) {
+        sort[x] <- sort[x] + 1
     }
+    sort <- sort(sort)
     control <- length(levels(as.factor(sort)))
   }
   # We rearange 'C' so that all sampled point are in different grid cells ('sort'):
   C <- xy[sort, ]
   xy.d1 <- dist(C)
-  C.list[[i]] <- C
   
   # Connectivity matrices (B):
   # **************************
   
-  Y.del <- tri2nb(C) 
+  Y.del <- tri2nb(C)
   Y.gab <- graph2nb(gabrielneigh(as.matrix(C), nnmult = 5), sym = TRUE)
   Y.rel <- graph2nb(relativeneigh(as.matrix(C), nnmult = 5), sym = TRUE)
   Y.mst <- mst.nb(xy.d1)
@@ -150,13 +136,13 @@ for (i in 1:nperm) {
 # Summary of the results:
 # ***********************
 
-for (i in 1:nrow(connect_param)-1) {
-  connect_param[i, 2] <- median(as.numeric(connect_param[i, c(8:(nperm + 7))]))
-  connect_param[i, 3] <- sd(as.numeric(connect_param[i, c(8:(nperm + 7))]))
-  connect_param[i, 4] <- median(as.numeric(connect_param[i, c(1008:(nperm + 1007))]))
-  connect_param[i, 5] <- sd(as.numeric(connect_param[i, c(1008:(nperm + 1007))])) 
-  connect_param[i, 6] <- median(as.numeric(connect_param[i, c(2008:(nperm + 2007))]))
-  connect_param[i, 7] <- sd(as.numeric(connect_param[i, c(2008:(nperm + 2007))]))
+for (j in 1:(nrow(connect_param)-1)) {
+  connect_param[j, 2] <- median(as.numeric(connect_param[j, c(8:(nperm + 7))]))
+  connect_param[j, 3] <- sd(as.numeric(connect_param[j, c(8:(nperm + 7))]))
+  connect_param[j, 4] <- median(as.numeric(connect_param[j, c(1008:(nperm + 1007))]))
+  connect_param[j, 5] <- sd(as.numeric(connect_param[j, c(1008:(nperm + 1007))])) 
+  connect_param[j, 6] <- median(as.numeric(connect_param[j, c(2008:(nperm + 2007))]))
+  connect_param[j, 7] <- sd(as.numeric(connect_param[j, c(2008:(nperm + 2007))]))
 }
 
 # Output of the results:
